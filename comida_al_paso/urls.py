d3fb192fe823f33effee09a8c7b5cf867730f803
Erica636 <erica.ansaloni36@gmail.com>
2025-11-20 11:54:15 -0300

    Update urls.py

--- comida_al_paso/urls.py
+++ comida_al_paso/urls.py
@@ -8,7 +8,11 @@ from rest_framework_simplejwt.views import (
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+
+    # API principal
     path('api/', include('api.urls')),
+
+    # JWT AUTH
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
